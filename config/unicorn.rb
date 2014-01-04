ea0fcfcf11f5d15396f7f5fcb8c9ce6d367222b1
khcr <kocher.ke@gmail.com>
2014-01-04 19:23:57 +0100

    deploy

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -9,5 +9,25 @@ worker_processes 2
 timeout 30
  
 before_exec do |server|
-  ENV['BUNDLE_GEMFILE'] = '/mnt/Hipstaweb-<%= RUBBER_ENV %>/current/Gemfile'
+  ENV['BUNDLE_GEMFILE'] = "/home/resj/apps/resj/current/Gemfile"
+end
+
+preload_app true
+
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
+
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      Process.kill("QUIT", File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+    end
+  end
+end
+
+after_fork do |server, worker|
+  ActiveRecord::Base.establish_connection
+  ActiveRecord::Base.verify_active_connections!
 end
