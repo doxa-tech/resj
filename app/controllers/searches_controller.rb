d9d34a9910bfa04458f166555fb55338a7e4d3e3
Noémien Kocher <nkcr.je@gmail.com>
2014-03-02 17:40:29 +0100

    admin update card design

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,7 +2,7 @@ class SearchesController < BaseController
 
 	def responsables
 		if params[:attr].in? %[firstname lastname email]
-			render json: Responsable.where("#{params[:attr]} like ?", "#{params[:term]}_%" ).uniq.pluck(params[:attr])
+			render json: Responsable.where("#{params[:attr]} like ?", "%#{params[:term]}%" ).uniq.pluck(params[:attr])
 		else
 			render nothing: true
 		end
@@ -10,26 +10,26 @@ class SearchesController < BaseController
 
 	def affiliations
 		if params[:attr].in? %[name]
-			render json: Affiliation.where("#{params[:attr]} like ?", "#{params[:term]}_%" ).pluck(params[:attr])
+			render json: Affiliation.where("#{params[:attr]} like ?", "%#{params[:term]}%" ).pluck(params[:attr])
 		else
 			render nothing: true
 		end
 	end
 
 	def tags
-		render json: Tag.where("name like ?", "#{params[:query]}_%" )
+		render json: Tag.where("name like ?", "%#{params[:query]}%" )
 	end
 
 	def actions
 		if params[:attr].in? %[name]
-			render json: Action.where("#{params[:attr]} like ?", "#{params[:term]}_%" ).pluck(params[:attr])
+			render json: Action.where("#{params[:attr]} like ?", "%#{params[:term]}%" ).pluck(params[:attr])
 		else
 			render nothing: true
 		end
 	end
 
 	def themes
-		render json: Theme.where("name like ?", "#{params[:query]}_%" )
+		render json: Theme.where("name like ?", "%#{params[:query]}%" )
 	end
 
 end
--- app/models/affiliation.rb
+++ app/models/affiliation.rb
@@ -2,5 +2,5 @@ class Affiliation < ActiveRecord::Base
 	has_many :card_affiliations, dependent: :destroy
 	has_many :cards, through: :card_affiliations
 
-	validates :name, presence: true, length: { maximum: 15 }
+	validates :name, presence: true, length: { maximum: 70 }
 end
