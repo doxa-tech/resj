755f9330bf51be84d65344994a8e18e63c450931
Noémien Kocher <nkcr.je@gmail.com>
2014-03-04 19:28:08 +0100

    shema.rb?

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140226165725) do
+ActiveRecord::Schema.define(version: 20140303185747) do
 
   create_table "access_tokens", force: true do |t|
     t.string   "token"
@@ -121,12 +121,8 @@ ActiveRecord::Schema.define(version: 20140226165725) do
     t.string   "banner"
     t.integer  "card_id"
     t.integer  "location_id"
-<<<<<<< HEAD
-    t.integer  "responsable_id"
-=======
     t.integer  "user_id"
     t.boolean  "visible"
->>>>>>> FETCH_HEAD
   end
 
   add_index "cards", ["card_type_id"], name: "index_cards_on_card_type_id"
