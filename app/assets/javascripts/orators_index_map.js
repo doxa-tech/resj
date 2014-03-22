d104c6fc0a628c1be2f2dc1b9e1fdedd3ebbefe7
Noémien Kocher <nkcr.je@gmail.com>
2014-03-22 17:06:05 +0100

    orator design

--- app/assets/javascripts/orators_index_map.js
+++ app/assets/javascripts/orators_index_map.js
@@ -1,5 +1,6 @@
 var gmap = {
   initialize: function() {
+    var infowindow = new google.maps.InfoWindow();
     var latlng = new google.maps.LatLng(46.57, 6.8794);
     var mapOptions = {
       center: latlng,
@@ -9,27 +10,26 @@ var gmap = {
     var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
     geocoder = new google.maps.Geocoder();
     google.maps.event.addDomListener(window, 'load');
-    return map;
+    return {"map":map,"infowindow": infowindow};
   },
-  addIcon: function(addr, map, text) {
+  addIcon: function(addr, map, text, infowindow) {
     geocoder.geocode( { 'address': addr}, function(results, status) {
       if (status == google.maps.GeocoderStatus.OK) {
         var coord = results[0].geometry.location;
-        
-        var infowindow = new google.maps.InfoWindow({
-          content: text
-        });
+
         var marker = new google.maps.Marker({
           position: coord,
           map: map,
-          title:"Hello World!"
+          title:"Click to open"
         });
         google.maps.event.addListener(marker, 'click', function() {
+          infowindow.close();
+          infowindow.setContent(text);
           infowindow.open(map,marker);
         });
 
       } else {
-        alert('Impossible de trouver ' + addr + " - " + status);
+        alert('Impossible de trouver ' + addr + " - " + status + " Merci de reporter le bug.");
       }
     });
   }
--- app/helpers/resources_helper.rb
+++ app/helpers/resources_helper.rb
@@ -1,10 +1,10 @@
 module ResourcesHelper
 
 	def type(subject)
-		if subject.resources.count > 1
+		if subject.documents.count > 1
 			return "folder"
 		else
-			return subject.resources.last.file.file.extension.downcase[0..2]
+			return subject.documents.last.file.file.extension.downcase[0..2]
 		end   
 	end
 	
