8b6f24ee13943ba4a002f86912a3b8618c234ea0
khcr <kocher.ke@gmail.com>
2014-04-01 00:22:04 +0200

    deploy

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -4,4 +4,8 @@ class Admin::BaseController < ApplicationController
 	before_action :authorize_create, only: [:new, :create]
 	before_action :authorize_modify, only: [:edit, :update, :destroy]
 
+	def url_options
+    { locale: (locale == :fr ? nil : locale), access: params[:access] }.merge(super)
+  end
+
 end
