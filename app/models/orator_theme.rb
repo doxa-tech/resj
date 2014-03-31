26d7a6a5600c67101d26182f6a5ea7afda16b36d
khcr <kocher.ke@gmail.com>
2014-03-31 18:03:26 +0200

    better themes

--- app/models/orator_theme.rb
+++ app/models/orator_theme.rb
@@ -1,4 +1,13 @@
 class OratorTheme < ActiveRecord::Base
   belongs_to :orator
   belongs_to :theme
+
+  after_create :reindex_tags
+  around_destroy :reindex_tags
+
+  private
+
+  def reindex_tags
+    orator.index!
+  end
 end
