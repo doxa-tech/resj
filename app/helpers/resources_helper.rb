6ecab2457756b231e61710200b9802049bda7d5a
Noémien Kocher <nkcr.je@gmail.com>
2014-04-18 11:38:22 +0200

    Document extension new rake

--- app/helpers/resources_helper.rb
+++ app/helpers/resources_helper.rb
@@ -4,7 +4,7 @@ module ResourcesHelper
 		if subject.documents.count > 1
 			return "folder"
 		else
-			return subject.documents.last.file.file.extension.downcase[0..2]
+			return subject.documents.last.extension
 		end   
 	end
 	
