b8678035f65aac7a42a78b23b14b68104290c7da
khcr <kocher.ke@gmail.com>
2014-04-28 22:28:11 +0200

    session

---
+++ app/assets/javascripts/sitewide/expiration.js
@@ -0,0 +1,9 @@
+$(function() {
+  var idleTimer;
+  function resetTimer(){
+    clearTimeout(idleTimer);
+    idleTimer = setTimeout(function() { window.location.reload(); }, 1800000);
+  }
+  $(document).on('click', resetTimer );
+  resetTimer();
+});
