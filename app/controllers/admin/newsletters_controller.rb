6d841b9bb7fd0e09408a032d1be328c497203383
khcr <kocher.ke@gmail.com>
2014-05-14 20:51:58 +0200

    emails

---
+++ app/controllers/admin/newsletters_controller.rb
@@ -0,0 +1,13 @@
+class Admin::NewslettersController < Admin::BaseController
+
+	def new
+	end
+
+	def create
+		# Mailer params[:content]
+		NewsMailer.news(params[:content], params[:subject]).deliver
+		Activity.create(action: "create", controller: "admin/newsletters")
+		redirect_to new_admin_newsletter_path, success: t('newsletter.admin.new.success')
+	end
+
+end
