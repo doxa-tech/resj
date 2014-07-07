1399b49d19fcf0d68f1bb21f3bcb5a6e89751f3d
Noémien Kocher <nkcr.je@gmail.com>
2014-07-08 00:36:00 +0200

    adjust order

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -21,7 +21,7 @@ class PagesController < BaseController
 	def resources
 		@page = Page.find_by_name('resources')
 		@article = Article.last
-		@subjects = Subject.last(4).reverse
+		@subjects = Subject.order(:updated_at).last(4).reverse
 	end
 
 	def help
