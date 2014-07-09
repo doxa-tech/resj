8a6132fcf17118ba366beff3dfbdb9cb5f6a471a
Noémien Kocher <nkcr.je@gmail.com>
2014-07-09 14:09:29 +0200

    admin header responsive

---
+++ app/assets/javascripts/sitewide/admin_header.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+	$('#admin_header .parent').find('ul').hide();
+	$('#admin_header .parent').find('.group_link').click(function(e) {
+		e.preventDefault();
+		$(this).next().slideToggle('1000', 'linear');
+	});
+	$('#admin_header .active').parents('.parent ul').show();
+
+	$('#admin_top_header').click(function(){
+		$('#admin_header').slideToggle();
+		window.onresize = function() {
+			if(window.innerWidth > 1200) {
+				$('#admin_header').show();
+			} else {
+				$('#admin_header').hide();
+			}
+		};
+	});
+});
