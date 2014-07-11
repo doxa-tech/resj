aa0cda1c3e445f0a47dfa7ed191f46fecccd7c9f
khcr <kocher.ke@gmail.com>
2014-07-11 15:43:17 +0200

    solr

--- app/models/article_theme.rb
+++ app/models/article_theme.rb
@@ -1,4 +1,13 @@
 class ArticleTheme < ActiveRecord::Base
   belongs_to :article
   belongs_to :theme
+
+  after_create :reindex_tags
+  around_destroy :reindex_tags
+
+  private
+
+  def reindex_tags
+    article.index!
+  end
 end
