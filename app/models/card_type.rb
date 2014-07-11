2f6d05c5bc74a42948633565ff7f22383787a6ab
khcr <kocher.ke@gmail.com>
2014-07-11 17:16:59 +0200

    Solr reindexing

--- app/models/card_type.rb
+++ app/models/card_type.rb
@@ -2,4 +2,17 @@ class CardType < ActiveRecord::Base
 	has_many :cards
 
 	validates :name, presence: true, length: { maximum: 30 }, uniqueness: true
+
+	after_save :saving_reindex
+  after_destroy :destroying_reindex
+
+  private
+
+  def saving_reindex
+		Sunspot.index(cards)
+  end
+
+  def destroying_reindex
+  	Card.reindex
+  end
 end
--- app/models/subject_theme.rb
+++ app/models/subject_theme.rb
@@ -1,13 +1,4 @@
 class SubjectTheme < ActiveRecord::Base
   belongs_to :subject
   belongs_to :theme
-
-  after_create :reindex_tags
-  around_destroy :reindex_tags
-
-  private
-
-  def reindex_tags
-    subject.index!
-  end
 end
