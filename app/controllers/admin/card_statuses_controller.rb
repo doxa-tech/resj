5f5375e8d48a3824ddc21d114bab70fd32cfd944
Noémien Kocher <nkcr.je@gmail.com>
2014-09-18 22:13:22 +0200

    Card status

--- app/controllers/admin/card_statuses_controller.rb
+++ app/controllers/admin/card_statuses_controller.rb
@@ -14,10 +14,20 @@ class Admin::CardStatusesController < Admin::BaseController
 	end
 
 	def update
-		if @card.update_attribute(:status_id, params[:card][:status_id])
-			redirect_to admin_card_statuses_path, success: t('card_status.admin.edit.success')
-		else
+		status = Status.find(params[:card][:status_id])
+		old_status = @card.status
+		@card.status = status
+		# don't do anything if not changed
+		if !@card.changed?
+			@card.errors.add(:status, "Le status n'a pas changé")
 			render 'edit'
+		else # if status is changed
+			if @card.save
+				CardMailer.changed_status(@card, status, old_status, params[:message]).deliver
+				redirect_to admin_card_statuses_path, success: t('card_status.admin.edit.success')
+			else
+				render 'edit'
+			end
 		end
 	end
 
