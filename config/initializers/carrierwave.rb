b19a612eb888aca6746036b237c9a2a68b6f674d
khcr <kocher.ke@gmail.com>
2014-09-28 23:26:13 +0200

    deploy

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,17 @@
-CarrierWave.configure do |config|
-  config.fog_credentials = {
-    :provider                         => 'Google',
-    :google_storage_access_key_id     => Rails.application.secrets.gcloud_id,
-    :google_storage_secret_access_key => Rails.application.secrets.gcloud_secret
-  }
-  config.fog_directory = Rails.application.secrets.gcloud_directory
+if Rails.env.development?
+	storage :file
+else
+	storage :fog
+	CarrierWave.configure do |config|
+	  config.fog_credentials = {
+	    :provider                         => 'Google',
+	    :google_storage_access_key_id     => Rails.application.secrets.gcloud_id,
+	    :google_storage_secret_access_key => Rails.application.secrets.gcloud_secret
+	  }
+	  config.fog_directory = Rails.application.secrets.gcloud_directory
 
-  CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
+	  CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
 
-  config.fog_public = true # changed in documents
+	  config.fog_public = true # changed in documents
+	end
 end
