7422ee79626532e15b7ba8db5a0afc196f362baf
Noémien Kocher <nkcr.je@gmail.com>
2014-10-02 22:31:01 +0200

    card location fix

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -30,8 +30,8 @@ class CardsController < BaseController
 
 	def show
 		@card = Card.find(params[:id])
-		js lat: @card.location.latitude
-		js lng: @card.location.longitude
+		js lat: @card.latitude
+		js lng: @card.longitude
 	end
 
 	def overview
