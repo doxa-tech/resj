42ffb588c77c4c394e53ab073380da20f68f0330
Noémien Kocher <nkcr.je@gmail.com>
2014-10-02 22:05:19 +0200

    article design

--- app/assets/javascripts/sitewide/pages/resources.js
+++ app/assets/javascripts/sitewide/pages/resources.js
@@ -14,5 +14,21 @@ PagesController.prototype.resources = function() {
 		e.preventDefault();
 		goToByScroll($('#orators'));  
 	});
+
+	var show = true;
+	if ($('article .text')[0].scrollHeight >  $('article .text').innerHeight()) {
+		$("#more-article").show();
+		$("#more-article").click(function(){
+			if(show) {
+				$("article .text").animate({"max-height": "100%"}, 500);
+				$("article .text button").html('Fermer <span class="fi-arrow-up">');
+				show = false;
+			} else {
+				$("article .text").animate({"max-height": "600px"}, 500);
+				$("article .text button").html('Lire la suite <span class="fi-arrow-down">');
+				show = true;
+			}
+		});
+	}
 	
 };
