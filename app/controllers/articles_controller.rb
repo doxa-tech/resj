f22aff61dea6c70f50f80442b103cce424ba6d48
khcr <kocher.ke@gmail.com>
2014-10-02 09:07:07 +0200

    show article & fix async option

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,12 @@
 class ArticlesController < ApplicationController
+	before_action :authorize_resource
 
 	def index
 		@articles = Article.order("created_at DESC")
+	end
+
+	def show
+		@article = Article.find(params[:id])
 	end
 	
 end
