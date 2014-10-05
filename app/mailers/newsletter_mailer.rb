2170b5726173dfcb54bf71a15cbb71a0fd0c2a97
Noémien Kocher <nkcr.je@gmail.com>
2014-10-05 16:29:24 +0200

    Newsletter mandrill option

--- app/mailers/newsletter_mailer.rb
+++ app/mailers/newsletter_mailer.rb
@@ -1,11 +1,20 @@
 class NewsletterMailer < BaseMailer
 
   # sends welcome
-  def news(content,subject,mails)
-  	@content = content
-    mail to:      mails,
-    			from: "\"Team Réseau Jeunesse\" <info@resj.ch>",
-         subject: subject
+  def news(content,subject,mails, mandrill)
+  	if mandrill
+	    mail to:      'chrikuhn@resj.ch, proginrene@gmail.com',
+	    		 from: "\"Team Réseau Jeunesse\" <info@resj.ch>",
+	         subject: subject,
+	         body:
+	    params({TEXT: content}) # variables
+	    template 'newsletter'  # template
+  	else
+	  	@content = content
+	    mail 	to:      mails,
+	    			from: "\"Team Réseau Jeunesse\" <info@resj.ch>",
+	         	subject: subject
+	  end
   end
 
 end
