3af1f02099251a4333c4801918b7eb1bce13c39e
Noémien Kocher <nkcr.je@gmail.com>
2014-10-26 15:38:46 +0100

    first version, waiting for upload

--- app/assets/javascripts/sitewide/cards/show.js
+++ app/assets/javascripts/sitewide/cards/show.js
@@ -6,6 +6,26 @@ CardsController.prototype.show = function() {
   load_google_map.loadScript('card_show.initialize', 
         [this.params['lat'], this.params['lng']], true);
 
+  $('.edit-banner').click(function(){
+    $('.edit-banner .edit-banner').css('display', 'block');
+    $('.edit-banner .tools').show();
+  });
+  $('body').click(function(e) {
+    if ($(e.target).closest('.edit-banner').length === 0) {
+      $('.edit-banner .tools').hide();
+      $('.edit-banner .edit-banner').css('display', 'none');
+    }
+  });
+  $('.edit-logo').click(function(){
+    $('.edit-logo .edit-logo').css('display', 'block');
+    $('.edit-logo .tools').show();
+  });
+  $('body').click(function(e) {
+    if ($(e.target).closest('.edit-logo').length === 0) {
+      $('.edit-logo .tools').hide();
+      $('.edit-logo .edit-logo').css('display', 'none');
+    }
+  });
 };
 
 var card_show = {
--- app/uploaders/banner_uploader.rb
+++ app/uploaders/banner_uploader.rb
@@ -11,7 +11,11 @@ class BannerUploader < CarrierWave::Uploader::Base
     storage :file
   end
 
-  process :resize_to_limit => [1200, 740]
+  version :original, :if => :bad_ratio?
+
+  process :check_size
+  process :check_ratio
+  process :resize_to_limit => [2500, 1200]
   process :quality => 90
   process :right_orientation
 
@@ -23,11 +27,40 @@ class BannerUploader < CarrierWave::Uploader::Base
     %w(jpg jpeg gif png)
   end
 
+  def check_size
+    w, h = ::MiniMagick::Image.open(file.file)[:dimensions]
+    if(w < 1000 || h < 180)
+      raise CarrierWave::ProcessingError.new("Votre image est trop petite. Une largeur d'au moins 1000px et une hauteur d'au moins 180px est nécessaire.")
+    end
+  end
+
+  def check_ratio
+    crop_ratio if bad_ratio?
+  end
+
   def right_orientation
     manipulate! do |img|
       img.auto_orient
       img
     end
   end
+
+  def bad_ratio?(picture=nil)
+    w, h = ::MiniMagick::Image.open(file.file)[:dimensions]
+    (w.to_f / h.to_f) < 2.1333
+  end
+
+  def crop_ratio
+    w, h = ::MiniMagick::Image.open(file.file)[:dimensions]
+    manipulate! do |img|
+      img.crop("#{w}x#{w.to_f/16*7.5}+#{0}+#{0}")
+      img
+    end
+  end
+
+  # NOTE about ratio and site
+  # ratio must not exceed 16 / 7.5
+  # size must not exceed 2500px wide (so 1200px height)
+  # size must be at least 1000px wide
 
 end
