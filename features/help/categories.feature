240f8a21fd74267775238ee73eb987e360f27313
khcr <kocher.ke@gmail.com>
2014-12-01 18:34:50 +0100

    update help tests

--- features/help/categories.feature
+++ features/help/categories.feature
@@ -5,15 +5,16 @@ Feature: Categories
   I want to see a list of categories
 
   Background:
-    Given there is a help page
+    Given there are many help pages
 
   Scenario: Visit main help page
     Given I am a visitor
     When I visit "/help"
     Then I should see links to the categories
 
+  @wip
   Scenario: Click on a category
     Given I am a visitor
     When I visit "/help"
     And I click the link "Images des groupes"
-    Then I should see links to help pages
+    Then I should see to help pages
--- features/step_definitions/help/categories_steps.rb
+++ features/step_definitions/help/categories_steps.rb
@@ -1,7 +1,15 @@
+Given /^there are many help pages$/ do
+  @page_1 = create(:help_page)
+  @page_2 = create(:help_page, name: "Mettre à jour son groupe")
+end
+
 Then /^I should see links to the categories$/ do
   expect(page).to have_link('Images des groupes')
 end
 
-Then /^I should see links to help pages$/ do
-  expect(page).to have_link(@page.name)
+Then /^I should see to help pages$/ do
+  expect(page).to content(@page_1.name)
+  expect(page).to content(@page_1.content)
+  expect(page).to content(@page_2.name)
+  expect(page).to content(@page_2.content)
 end
