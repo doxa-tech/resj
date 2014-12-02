32c586a3ae93e4f9ddcab8dfc5b0bc4bf997ed58
Noémien Kocher <nkcr.je@gmail.com>
2014-12-02 21:32:12 +0100

    change max char in help category (damn!)

--- app/models/help_page.rb
+++ app/models/help_page.rb
@@ -1,7 +1,7 @@
 class HelpPage < ActiveRecord::Base
   belongs_to :category, class_name: HelpCategory, foreign_key: "help_category_id"
 
-  validates :name, presence: true, length: { maximum: 30 }
+  validates :name, presence: true, length: { maximum: 60 }
   validates :content, presence: true
   validates :help_category_id, presence: true
 
