b508212e14ff7efb56cc707673a6b75524ad8bc6
khcr <kocher.ke@gmail.com>
2014-12-09 22:50:01 +0100

    bin config

--- config/deploy.rb
+++ config/deploy.rb
@@ -51,7 +51,9 @@ set :linked_files, %w{config/database.yml config/secrets.yml config/sunspot.yml}
 set :config_files, %w{config/unicorn.rb}
 
 # Default value for linked_dirs is []
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+set :bundle_binstubs, nil
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
