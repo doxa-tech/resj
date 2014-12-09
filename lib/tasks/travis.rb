ab9847655f32ae744f9b2745c6da2ec86ce73e9f
Noémien Kocher <nkcr.je@gmail.com>
2014-12-09 22:31:03 +0100

    made travis rake task

---
+++ lib/tasks/travis.rb
@@ -0,0 +1,13 @@
+namespace :travis do 
+
+	desc "Set up rspec & cucumber for travis"
+	task :travis do
+		`curl -sSL https://raw.githubusercontent.com/moliware/travis-solr/master/travis-solr.sh | SOLR_VERSION=4.6.1 bash`
+	  ["rspec spec", "rake jasmine:ci", "rake cucumber"].each do |cmd|
+	    puts "Starting to run #{cmd}..."
+	    system("export DISPLAY=:99.0 && bundle exec #{cmd}")
+	    raise "#{cmd} failed!" unless $?.exitstatus == 0
+	  end
+	end
+
+end
