e8f68d03eb2b00600be3bedcda296846daea5664
Noémien Kocher <nkcr.je@gmail.com>
2014-12-10 15:07:24 +0100

    removed chrome driver

--- features/support/env.rb
+++ features/support/env.rb
@@ -64,9 +64,9 @@ Cucumber::Rails::Database.javascript_strategy = :truncation
 #
 #
 #
-Capybara.register_driver :chrome do |app|
-  Capybara::Selenium::Driver.new(app, :browser => :chrome)
-end
+# Capybara.register_driver :chrome do |app|
+#   Capybara::Selenium::Driver.new(app, :browser => :chrome)
+# end
 
 Capybara.javascript_driver = :chrome
 
