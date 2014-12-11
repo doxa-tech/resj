b912fe525e9cd92f44ef16614ffff8840e7ff1ca
Noémien Kocher <nkcr.je@gmail.com>
2014-12-11 08:50:35 +0100

    forgot to remove the require

--- features/support/env.rb
+++ features/support/env.rb
@@ -8,9 +8,6 @@ require 'cucumber/rails'
 require 'rake' # nkcr
 require 'email_spec/cucumber'
 
-require 'capybara/cucumber'
-require 'sauce/cucumber'
-
 # Capybara defaults to CSS3 selectors rather than XPath.
 # If you'd prefer to use XPath, just uncomment this line and adjust any
 # selectors in your step definitions to use the XPath syntax.
