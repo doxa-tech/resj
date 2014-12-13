32aa64a79493c4808bcc75a8ef78b3bb08373770
khcr <kocher.ke@gmail.com>
2014-12-13 16:50:53 +0100

    sunspot auth

--- config/initializers/sunspot.rb
+++ config/initializers/sunspot.rb
@@ -1,5 +1,1 @@
-Sunspot.configure do |config| 
-
-  config.solr.url = "http://tomcat:my_little_poney@localhost:8983/solr"
-
-end
+Sunspot.config.solr.url = "http://tomcat:my_little_poney@localhost:8983/solr"
