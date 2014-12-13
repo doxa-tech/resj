624ae2964f8c777ac01e8c4fed3be91dff761f4c
khcr <kocher.ke@gmail.com>
2014-12-13 17:00:13 +0100

    config sunspot auth

--- config/initializers/sunspot.rb
+++ config/initializers/sunspot.rb
@@ -1,1 +1,3 @@
-Sunspot.config.solr.url = "http://tomcat:my_little_poney@localhost:8080/solr"
+if !Rails.env.development? && !Rails.env.test?
+  Sunspot.config.solr.url = "http://#{Rails.application.secrets.tomcat_user}:#{Rails.application.secrets.tomcat_pswd}@localhost:8080/solr"
+end
