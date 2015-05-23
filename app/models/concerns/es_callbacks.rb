a20e1fc5859667bbfa16587d8209e764a1acae30
khcr <kocher.ke@gmail.com>
2015-05-23 23:48:40 +0200

    remove debug lines

--- app/models/concerns/es_callbacks.rb
+++ app/models/concerns/es_callbacks.rb
@@ -9,10 +9,8 @@ module EsCallbacks
   def index_with_belongs_to(*args)
     fields = args.map { |e| self.class.reflect_on_association(e).foreign_key }
     if (fields - __elasticsearch__.instance_variable_get(:@__changed_attributes).keys).length < fields.length
-      puts "BELONGS"
       __elasticsearch__.index_document
     else
-      puts "Update"
       __elasticsearch__.update_document
     end
   end
