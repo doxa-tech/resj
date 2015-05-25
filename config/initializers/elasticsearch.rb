0e6fb5839564703fde7a43cff9e9ac44eb4690d8
khcr <kocher.ke@gmail.com>
2015-05-25 21:36:56 +0200

    remove unnecessary .css & fix elasticsearch config

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,3 +1,3 @@
-CONFIG = YAML.load_file("#{Rails.root}/config/deploy/config/elasticsearch.yml")[Rails.env]
+CONFIG = YAML.load_file("#{Rails.root}/config/elasticsearch.yml")[Rails.env]
 
 Elasticsearch::Model.client = Elasticsearch::Client.new host: "#{CONFIG['host']}:#{CONFIG['port']}"
