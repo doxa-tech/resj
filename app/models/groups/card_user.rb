c7cfa2a35cace71fa532c66bdea576ddc880c13a
khcr <kocher.ke@gmail.com>
2015-06-27 12:21:51 +0200

    validations fix

--- app/models/groups/card_user.rb
+++ app/models/groups/card_user.rb
@@ -1,4 +1,15 @@
 class CardUser < ActiveRecord::Base
   belongs_to :card
   belongs_to :user
+
+  validate :owner?
+
+  private
+
+  def owner?
+    if card.user == user
+      errors.add(:card_user, "tente d'affilier un groupe à son priopriétaire.")
+    end
+  end
+
 end
