51bcb73ab16a10edc1c4c86d6a0d6672c6a37549
khcr <kocher.ke@gmail.com>
2015-08-31 15:57:09 +0200

    miscellaneous

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -2,7 +2,7 @@ class SubjectsController < BaseController
 	before_action :authorize_resource
 
 	def index
-  	@subjects = Subject.search(params).includes(:themes)
+  	@subjects = Subject.search(params).includes(:themes, :documents)
 	end
 
 	def show
