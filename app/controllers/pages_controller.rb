2cc33312e9823b8b3a5f28096bf1e8de3e608543
khcr <kocher.ke@gmail.com>
2015-09-13 23:32:06 +0200

    fix pie charts

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,7 +7,7 @@ class PagesController < BaseController
 		@card_count = Card.active.count
 		@orator_count = Orator.count
 		@doc_count = Document.count
-		@user_count = 20
+		@user_count = User.users.count
 	end
 
 	def resj
