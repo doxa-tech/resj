6486237a7c37490f5bc78a9b2fb9352e59faf3a9
khcr <kocher.ke@gmail.com>
2015-10-23 15:13:35 +0200

    errors handler

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,11 +1,22 @@
 class ErrorsController < BaseController
-  def file_not_found
+  def page_not_found
+    respond_to do |format|
+      format.html { render 'page_not_found', status: 404 }
+      format.all { render nothing: true, status: 404 }
+    end
   end
 
   def unprocessable
+    respond_to do |format|
+      format.html { render 'unprocessable', status: 422 }
+      format.all { render nothing: true, status: 422 }
+    end
   end
 
   def internal_server_error
-  	
+    respond_to do |format|
+      format.html { render 'internal_server_error', status: 500 }
+      format.all { render nothing: true, status: 500 }
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  match '/404', to: 'errors#file_not_found', via: :all
+  match '/404', to: 'errors#page_not_found', via: :all
   match '/422', to: 'errors#unprocessable', via: :all
   match '/500', to: 'errors#internal_server_error', via: :all
 
