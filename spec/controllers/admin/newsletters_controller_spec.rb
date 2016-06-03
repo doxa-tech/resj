e69d6c783db8bc1f67bea1d013c8d83038556634
Noémien Kocher <nkcr.je@gmail.com>
2016-06-03 21:13:34 +0200

    remove NewsletterMailer tests

--- spec/controllers/admin/newsletters_controller_spec.rb
+++ spec/controllers/admin/newsletters_controller_spec.rb
@@ -12,19 +12,21 @@ RSpec.describe Admin::NewslettersController, :type => :controller do
 			}
 		end
 
-		it "should send an email with the right params" do
-			@params[:emails] =  create_list(:user_list, 5).map(&:email)
-			expect(NewsletterMailer).to receive(:news).with(@params[:subject], array_including(@params[:emails])).and_call_original
-			post :create, **@params
-		end
-
-		it "should not send an email to an user who does not want" do
-			valid_user = create(:user); invalid_user = create(:user, email: "invalid@user.com")
-			invalid_user.newsletters = Newsletter.none
-			@params[:emails] = [valid_user.email, invalid_user.email]
-			expect(NewsletterMailer).to receive(:news).with(@params[:subject], [valid_user.email]).and_call_original
-			post :create, **@params
-		end
+		# Those tests could be handy if we use back NewsletterMailer one day
+		#
+		# it "should send an email with the right params" do
+		# 	@params[:emails] =  create_list(:user_list, 5).map(&:email)
+		# 	expect(NewsletterMailer).to receive(:news).with(@params[:subject], array_including(@params[:emails])).and_call_original
+		# 	post :create, **@params
+		# end
+		#
+		# it "should not send an email to an user who does not want" do
+		# 	valid_user = create(:user); invalid_user = create(:user, email: "invalid@user.com")
+		# 	invalid_user.newsletters = Newsletter.none
+		# 	@params[:emails] = [valid_user.email, invalid_user.email]
+		# 	expect(NewsletterMailer).to receive(:news).with(@params[:subject], [valid_user.email]).and_call_original
+		# 	post :create, **@params
+		# end
 
 	end
 
