727075b5246c4ac0386d882a71f2876d712d5965
Noémien Kocher <nkcr.je@gmail.com>
2020-01-05 21:38:10 +0100

    Excludes mapbox-gl from the babel transpiler

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,12 @@
 const { environment } = require('@rails/webpacker')
 
+// Excludes Mapbox-GL from Babel transpilker
+const nodeModulesLoader = environment.loaders.get('nodeModules')
+if (!Array.isArray(nodeModulesLoader.exclude)) {
+  nodeModulesLoader.exclude = (nodeModulesLoader.exclude == null)
+    ? []
+    : [nodeModulesLoader.exclude]
+}
+nodeModulesLoader.exclude.push(/mapbox-gl/)
+
 module.exports = environment
