a814ca759bd4ff7014e64ae3c1e4781bc72b489e
Noémien Kocher <nkcr.je@gmail.com>
2021-10-08 16:31:36 +0200

    Adds responsive home page

---
+++ app/javascript/controllers/header_controller.js
@@ -0,0 +1,15 @@
+import { Controller } from "stimulus"
+
+export default class SearchController extends Controller {
+  static targets = ["container", "open", "close"]
+
+  close() {
+    this.containerTarget.style.display = "none";
+    this.openTarget.style.display = "block";
+  }
+
+  open() {
+    this.containerTarget.style.display = "flex";
+    this.openTarget.style.display = "none";
+  }
+}
