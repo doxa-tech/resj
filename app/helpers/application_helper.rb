fd0559067599b6df647205cc0e9b23f07a767338
Keran Kocher <kocher.ke@gmail.com>
2021-10-23 11:46:06 +0200

    add a popup confirmation for delete account and orator profile

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,10 @@ module ApplicationHelper
       record.send(enum))
   end
 
+  def link_to_with_confirmation(text, url, **args)
+    render "confirmation", url: url, action_text: text, args: args do
+      yield
+    end
+  end
+
 end
---
+++ app/javascript/controllers/popup_controller.js
@@ -0,0 +1,14 @@
+import { Controller } from "stimulus"
+
+export default class PopupController extends Controller {
+  static targets = ["content"]
+
+  display() {
+    this.contentTarget.classList.add("active");
+  }
+
+  close() {
+    this.contentTarget.classList.remove("active");
+  }
+
+}
