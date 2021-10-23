8f6e2b34ba788e6662160bbedfcd08c8c94febe1
Noémien Kocher <nkcr.je@gmail.com>
2021-10-23 20:05:41 +0200

    Adds orator creation link

---
+++ app/javascript/controllers/faq_controller.js
@@ -0,0 +1,16 @@
+import { Controller } from "stimulus"
+
+// This controller opens the summary and sets it a class based on the hash found
+// in the URL.
+export default class FaqController extends Controller {
+  initialize() {
+    if (window.location.hash) {
+      const id = window.location.hash.substring(1);
+      const details = document.getElementById(id);
+      if (details) {
+        details.classList.add("focus");
+        details.open = true;
+      }
+    }
+  }
+}
