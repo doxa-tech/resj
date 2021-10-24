5d0e885e5695c624503213f7ffa6da8dbc979eb8
Noémien Kocher <nkcr.je@gmail.com>
2021-10-24 23:47:56 +0200

    Adds email setting for production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,6 +66,23 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.default_url_options = { :host => "https://reseaujeunesse.ch" }
+  config.action_mailer.asset_host = "https://reseaujeunesse.ch"
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.smtp_settings = {
+    :user_name => Rails.application.credentials.production[:mailer][:user_name],
+    :password  => Rails.application.credentials.production[:mailer][:password],
+    :domain => Rails.application.credentials.production[:mailer][:domain],
+    :address   => Rails.application.credentials.production[:mailer][:address],
+    :port      => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
