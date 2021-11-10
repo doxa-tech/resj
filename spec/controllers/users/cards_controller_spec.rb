cfafcb38682c1fd04d7c6e0d417b7473ff0426d2
Keran Kocher <kocher.ke@gmail.com>
2021-11-10 23:51:38 +0100

    test that every card has a last_updated value and that it is updated

--- spec/controllers/users/cards_controller_spec.rb
+++ spec/controllers/users/cards_controller_spec.rb
@@ -6,7 +6,7 @@ module Users
 
     describe "#transfer" do
 
-      it "transfer the card to another user" do
+      it "transfers the card to another user" do
         @card = create(:card, status: nil)
         request.cookies[:remember_token] = @card.user.remember_token
         email = "another@bar.com"
@@ -23,6 +23,21 @@ module Users
         post :transfer, params: { id: @card.id, email: "nil@bar.com" }
         @card.reload
         expect(@card.user).to eq @user
+      end
+
+    end
+
+    describe "#update" do
+
+      it "updates the last_updated time" do
+        @card = create(:card)
+        request.cookies[:remember_token] = @card.user.remember_token
+        last_updated = @card.last_updated
+        travel 1.day do
+          patch :update, params: { id: @card.id, card: @card.attributes }
+        end
+        @card.reload
+        expect(@card.last_updated).to be > last_updated
       end
 
     end
