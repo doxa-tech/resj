8eec8419015bcb2453f27c3e6e8c09a9f672296f
Noémien Kocher <nkcr.je@gmail.com>
2021-11-14 18:01:33 +0100

    Adds design for admin/cards

--- app/controllers/admin/cards_controller.rb
+++ app/controllers/admin/cards_controller.rb
@@ -2,7 +2,7 @@ class Admin::CardsController < Admin::BaseController
   load_and_authorize
 
   def index
-    @table = CardTable.new(self, @cards, search: true)
+    @table = CardTable.new(self, @cards.where.not(status: "incomplete"), search: true)
   end
 
   def edit
