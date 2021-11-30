725137a1c4c8d1122d35340e117dc989dde26a6c
Keran Kocher <kocher.ke@gmail.com>
2021-11-30 22:25:46 +0100

    fix the entrypoint script

--- .deploy/entrypoint.sh
+++ .deploy/entrypoint.sh
@@ -4,12 +4,13 @@ set -o errexit
 set -o pipefail
 
 if [[ -z "${RAILS_MASTER_KEY}" ]]; then
+  echo "Set the RAILS_MASTER_KEY environment variable before starting the application."
+else
   cd $WEBROOT/ROOT
   echo "Running migrations..."
   rake db:prepare
   echo "Done"
 
   /etc/rc.d/init.d/nginx start
-else
-  echo "Set the RAILS_MASTER_KEY environment variable before starting the application."
+  echo "Server started"
 fi
