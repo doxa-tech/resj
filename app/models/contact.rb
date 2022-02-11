69959a1957adc1c1de514cfc4ad4883a633d9000
Keran Kocher <kocher.ke@gmail.com>
2022-02-11 17:18:27 +0100

    add the verification to the contact form

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -9,9 +9,19 @@ class Contact
 
   validates :message, :email, :verification, presence: true
   validates :email, format: { :with => /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/ }
+  validate :verification_is_correct
 
   def destination
-    Rails.env.production? ? @destination : "kocher.ke@gmail.com"
+    Rails.env.development? ? "kocher.ke@gmail.com" : @destination
+  end
+
+  private
+
+  # the verification question is 2+2
+  def verification_is_correct
+    if verification.strip != "4"
+      errors.add(:base, "La vérification a échouée")
+    end
   end
 
 end
