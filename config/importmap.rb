997d4b17120d1b8a1e54544181209ed833123d0c
Keran Kocher <2395320+khcr@users.noreply.github.com>
2023-12-02 18:18:32 +0100

    add Rails UJS

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,4 @@ pin "choices.js", to: "https://ga.jspm.io/npm:choices.js@9.1.0/public/assets/scr
 pin "mapbox-gl", to: "https://ga.jspm.io/npm:mapbox-gl@2.15.0/dist/mapbox-gl.js", preload: true
 pin "process", to: "https://ga.jspm.io/npm:@jspm/core@2.0.1/nodelibs/browser/process-production.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "@rails/ujs", to: "https://ga.jspm.io/npm:@rails/ujs@7.1.2/app/assets/javascripts/rails-ujs.esm.js"
